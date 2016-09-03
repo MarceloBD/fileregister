73e093fa0309c94edef1778364abf6120ce091d9
Marcelo Bertoldi Diani <marcelodianib@gmail.com>
2016-09-02 21:44:36 -0300

    [Fixed] Input bugs
    Inputs bigs than the size of each field have been fixed

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ PRICE   (Price of the product)
 
 #include "main.h"
 
-void main()
+void main( )
 {
     interface();
 }
@@ -27,18 +27,19 @@ void interface()
 
     printf("\n1- Want to make a registration?\n2- Want to take a register?\n3- Want to see the registers?\n\n>>");
 
-    scanf("%s" , &optionMain);
+    scanf("%s", &optionMain);
 
     if(optionMain == '1')
     {
         system("cls");
         newRegister();
+        interface();
     }
 
     else if (optionMain == '2')
     {
         printf("\nType the number of register: ");
-        scanf("%d" , &rgtNumber);
+       scanf("%d" , &rgtNumber);
         system("cls");
         readRegister(rgtNumber);
 
@@ -66,48 +67,55 @@ void newRegister()
     struct registerFields newRgt;
 
     if(!exists(FILENAME)){
-        FILE *create = fopen(FILENAME , "w+" );
+        FILE *create = fopen(FILENAME , "wb" );
         fclose(create);
     }
 
-    FILE *fp = fopen( FILENAME ,"r+" );
+    FILE *fp = fopen( FILENAME ,"rb+" );
 
     fseek(fp, 0, SEEK_END);
     length = ftell(fp);
 
     RRN = length/rgtLength_t;
 
+    int j;
     printf("\nName (10): ");
-    scanf("%s",&newRgt.name);
-    fseek ( fp , 0+rgtLength_t*RRN , SEEK_SET );
-    fwrite(newRgt.name , 1 , sizeof(newRgt.name), fp);
+    scanf("%10s", &newRgt.name);
+    int c;
+    while ((c = getchar()) != EOF && c != '\n') ;
+
+    fseek ( fp , 0 + rgtLength_t * RRN , SEEK_SET );
+    fwrite(newRgt.name , 1 , 10, fp);
 
     printf("Brand (10): ");
-    scanf("%s",&newRgt.brand);
-    fseek ( fp , 10+rgtLength_t*RRN , SEEK_SET );
+    scanf("%10s" , &newRgt.brand);
+    while ((c = getchar()) != EOF && c != '\n') ;
+    fseek ( fp , 10+rgtLength_t * RRN , SEEK_SET );
     fwrite(newRgt.brand , 1 , sizeof(newRgt.brand), fp);
 
 
     printf("Ean13 (13): ");
-    scanf("%s",&newRgt.ean13);
-    fseek ( fp , 20+rgtLength_t*RRN, SEEK_SET );
+    scanf("%13s" , &newRgt.ean13);
+   while ((c = getchar()) != EOF && c != '\n') ;
+    fseek ( fp , 20 + rgtLength_t * RRN , SEEK_SET );
     fwrite(newRgt.ean13 , 1 , sizeof(newRgt.ean13), fp);
 
 
     printf("Price (4): ");
-    scanf("%s",&newRgt.price);
-    fseek ( fp , 33+rgtLength_t*RRN, SEEK_SET );
+    scanf("%4s" , &newRgt.price);
+    while ((c = getchar()) != EOF && c != '\n') ;
+    fseek ( fp , 33 + rgtLength_t * RRN , SEEK_SET );
 
     fwrite(newRgt.price , 1 , sizeof(newRgt.price), fp);
 
-    fseek ( fp , rgtLength_t*(RRN+1), SEEK_SET );
+    fseek ( fp , rgtLength_t * (RRN+1) , SEEK_SET );
     fprintf(fp, "%c", '0');
     fclose(fp);
 
     system("cls");
     printf("\nYour register has been realized with RRN = %d\n",RRN);
 
-    interface();
+
 }
 
 void readRegister(int RRN){
@@ -115,12 +123,12 @@ void readRegister(int RRN){
     struct registerFields rgtFound;
     int i;
 
-    FILE *fp=fopen( FILENAME, "r+" );
-    fseek(fp, 0, SEEK_END);
+    FILE *fp = fopen( FILENAME, "rb+" );
+    fseek(fp , 0 , SEEK_END);
     int length = ftell(fp);
-    length=(length/rgtLength_t)-1;
+    length = (length / rgtLength_t) - 1;
 
-    if(RRN>length){
+    if(RRN > length){
         printf("\nInvalid register \n");
         interface();
         return;
@@ -128,42 +136,46 @@ void readRegister(int RRN){
 
     printf("----------------\n");
 
-    fseek(fp,0+RRN*rgtLength_t,SEEK_SET);
+    fseek(fp , 0 + RRN * rgtLength_t , SEEK_SET);
     printf("Name: ");
-    fread(rgtFound.name , 1 , sizeof(rgtFound.name), fp);
-    printf("%s",rgtFound.name);
+    fread(rgtFound.name , 1 , 10, fp);
+    rgtFound.name[10]=0;
+    printf("%s" , rgtFound.name);
 
-    fseek(fp,10+RRN*rgtLength_t,SEEK_SET);
+
+    fseek(fp , 10 + RRN * rgtLength_t , SEEK_SET);
     printf("\nBrand: ");
     fread(rgtFound.brand , 1 , sizeof(rgtFound.brand), fp);
-    printf("%s",rgtFound.brand);
+    rgtFound.brand[10]=0;
+    printf("%s" , rgtFound.brand);
 
 
     printf("\nEan13: ");
-    fseek(fp,20+RRN*rgtLength_t,SEEK_SET);
+    fseek(fp, 20 + RRN * rgtLength_t, SEEK_SET);
 
     fread(rgtFound.ean13 , 1 , sizeof(rgtFound.ean13), fp);
-    printf("%s",rgtFound.ean13);
+    rgtFound.ean13[13]=0;
+    printf("%s" , rgtFound.ean13);
 
 
-    fseek(fp,33+RRN*rgtLength_t,SEEK_SET);
+    fseek(fp,33 + RRN * rgtLength_t , SEEK_SET);
     printf("\nPrice: ");
 
     fread(rgtFound.price , 1 , sizeof(rgtFound.price), fp);
-    printf("%s",rgtFound.price);
+    rgtFound.price[4]=0;
+    printf("%s" , rgtFound.price);
 
     printf("\n----------------");
 
     fclose(fp);
   }
 
-
 int fileLength()
 {
-        FILE *fp=fopen( FILENAME, "r+" );
-        fseek(fp, 0, SEEK_END);
+        FILE *fp = fopen( FILENAME, "r+" );
+        fseek(fp , 0 , SEEK_END);
         int length = ftell(fp);
-        length=(length/rgtLength_t)-1;
+        length = (length / rgtLength_t) - 1;
         return length;
 }
 
